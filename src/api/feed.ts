223e4c6b7644b66fb5095713e4278c66c19b8430
Roman U. (theDesConnet) <uporov.201125@gmail.com>
2025-11-03 17:08:38 +0700

    docs(feed): documenting endpoints in feed

--- src/api/feed.ts
+++ src/api/feed.ts
@@ -9,14 +9,38 @@ import {
 export class Feed {
     public constructor(private readonly client: Anixart) { }
 
-    public async news(page: number, options?: IBaseApiParams): Promise<IPageableResponse<IArticle>> {
-        return await this.client.call<DefaultResult, IPageableResponse<IArticle>>({ path: `/feed/news/all/${page}`, ...options });
-    }
-
+    /**
+     * Получение постов с каналов на которые подписан
+     * 
+     * Возвращает список {@link IArticle} внутри {@link IPageableResponse}.
+     * 
+     * Возможные ответы API в виде enum смотреть здесь {@link DefaultResult}
+     * 
+     * @param page - Номер страницы
+     * @param options - Дополнительные параметры
+     * @returns Список постов
+     * 
+     * @example
+     * const result = await client.endpoints.feed.my(0);
+     */
     public async my(page: number, options?: IBaseApiParams): Promise<IPageableResponse<IArticle>> {
         return await this.client.call<DefaultResult, IPageableResponse<IArticle>>({ path: `/feed/my/all/${page}`, ...options });
     }
 
+    /**
+     * Получение последних выложенных постов
+     * 
+     * Возвращает список {@link IArticle} внутри {@link IPageableResponse}.
+     * 
+     * Возможные ответы API в виде enum смотреть здесь {@link DefaultResult}
+     * 
+     * @param page - Номер страницы
+     * @param options - Дополнительные параметры
+     * @returns Список постов
+     * 
+     * @example
+     * const result = await client.endpoints.feed.latest(0);
+     */
     public async latest(page: number, options?: IBaseApiParams): Promise<IPageableResponse<IArticle>> {
         return await this.client.call<DefaultResult, IPageableResponse<IArticle>>({ path: `/article/latest/all/${page}`, method: "POST", ...options });
     }
