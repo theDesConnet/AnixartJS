f749e538f80571fd51d37de47b2bd3a047cfe8e7
Roman U. (theDesConnet) <uporov.201125@gmail.com>
2025-12-02 19:47:27 +0700

    fix(LinkParser): Fix AnilibriaParser

--- src/utils/LinkParser.ts
+++ src/utils/LinkParser.ts
@@ -2,6 +2,8 @@
  * Парсер различных источников
  */
 
+import { ChildProcessWithoutNullStreams } from "child_process";
+
 /**
  * KodikParser
  * Парсер источника Kodik
@@ -122,86 +124,184 @@ export class KodikParser {
  */
 
 /**
- * Полученный обьект Файлов
+ * Тайтл
  */
-export interface AniLibriaFile {
-    id: string,
-    skip: boolean,
-    file: string,
-    title: string,
-    poster: string,
-    download: string
+export interface AniLibriaAnime {
+    id: number,
+    type: {
+        value: string,
+        description: string
+    },
+    year: number,
+    name: {
+        main: string,
+        english: string,
+        alternative: string
+    },
+    alias: string,
+    season: {
+        value: string,
+        description: string
+    },
+    poster: {
+        src: string,
+        preview: string,
+        thumbnail: string,
+        optimized: {
+            src: string,
+            preview: string,
+            thumbnail: string
+        }
+    },
+    fresh_at: string,
+    created_at: string,
+    updated_at: string,
+    is_ongoing: boolean,
+    age_rating: {
+        value: string,
+        description: string,
+        label: string,
+        is_adult: boolean
+    },
+    publish_day: {
+        value: string,
+        description: string
+    },
+    description: string
+    notification: any,
+    episodes_count: number | null,
+    external_player: any,
+    is_in_production: boolean,
+    is_blocked_by_geo: boolean,
+    is_blocked_by_copyrights: boolean,
+    added_in_users_favorites: number,
+    average_duration_of_episode: any,
+    added_in_planned_collection: number,
+    added_in_watched_collection: number,
+    added_in_watching_collection: number,
+    added_in_postponed_collection: number,
+    added_in_abandoned_collection: number,
+    genres: AniLibriaGenre[],
+    members: AniLibriaMember[],
+    sponsor: {
+        id: string,
+        title: string,
+        description: string,
+        url_title: string,
+        url: string
+    },
+    episodes: AniLibriaEpisode[]
 }
 
 /**
- * Ссылка на файл
+ * Жанр
  */
-export interface AniLibriaLink {
-    src: string
+export interface AniLibriaGenre {
+    id: number,
+    name: string,
+    image: {
+        preview: string,
+        thumbnail: string,
+        optimized: {
+            preview: string,
+            thumbnail: string
+        }
+    },
+    total_releases: number
 }
 
 /**
- * Обьект файла
+ * Участник Anilibria
  */
-export interface AniLibriaFileObject {
+export interface AniLibriaMember {
     id: string,
-    skip: boolean,
-    file: Record<string, AniLibriaLink>,
-    title: string,
-    poster: string,
-    download: string
+    role: {
+        value: string,
+        description: string
+    },
+    nickname: string,
+    user: any
+}
+
+/**
+ * Эпизод
+ */
+export interface AniLibriaEpisode {
+    id: string,
+    name: string | null,
+    ordinal: number,
+    opening: {
+        stop: number | null,
+        start: number | null
+    },
+    ending: {
+        stop: number | null,
+        start: number | null
+    },
+    preview: {
+        src: string,
+        preview: string,
+        thumbnail: string,
+        optimized: {
+            src: string,
+            preview: string,
+            thumbnail: string
+        }
+    },
+    hls_480: string,
+    hls_720: string,
+    hls_1080: string,
+    duration: number,
+    rutube_id: any,
+    youtube_id: any,
+    updated_at: string,
+    sort_order: number,
+    release_id: number,
+    name_english: string | null
 }
 
 /**
  * Возвращаемый обьект
  */
 export interface AniLibriaReturnObject {
-    files: AniLibriaFileObject[]
+    [key: string]: {
+        src: string
+    }
 }
 
 /**
  * Класс парсера анилибрии
  */
 export class AniLibriaParser {
-    private static _baseAniLibriaDomain = 'anixart.libria.fun'
-    private static _endpointUrl = '/public/iframe.php'
+    private static _baseAniLibriaDomain = 'anilibria.top'
+    private static _endpointUrl = '/api/v1/anime/releases'
 
-    public static settingsPattern = new RegExp(/\[{.*?}\]/g);
-    public static filesPattern =  new RegExp(/\[.*?\].*?(,|isAuthorized=\d)/g)
+    public static idPattern = new RegExp(/id=(?<id>\d+)/g);
+    public static episodePattern =  new RegExp(/ep=(?<ep>\d+)/g)
 
     public static async getDirectLinks(link: string): Promise<AniLibriaReturnObject | null> {
-        const request = await fetch(link);
+        const id = this.idPattern.exec(link)?.groups?.id;
+        const episode = this.episodePattern.exec(link)?.groups?.ep;
 
-        let body = await request.text();
-        let match = this.settingsPattern.exec(body);
+        const request = await fetch(`https://${this._baseAniLibriaDomain}${this._endpointUrl}/${id}`);
 
-        if (match) {
-            let originalObject: AniLibriaFile[] = JSON.parse(match[0]);
+        let body = await request.json() as AniLibriaAnime;
 
-            let returnedObject: AniLibriaReturnObject = {
-                files: [],
-            }
-
-            for (let x of originalObject) {
-                let res: AniLibriaFileObject = {
-                    ...x,
-                    file: {}
-                }
+        if (!body || typeof episode != 'string') return null;
+        const ep = body.episodes.find(e => e.ordinal == parseInt(episode!)) ?? null;
         
-                let filesMatch = x.file.match(this.filesPattern);
-
-                for (let y of filesMatch!) {
-                    let link = y.replace('[', '').split(']');
-
-                    res.file[link[0].replace("p", "")] = {
-                        src: link[1]
-                    }
+        if (ep) {
+            return {
+                "1080": {
+                    src: ep.hls_1080
+                },
+                "720": {
+                    src: ep.hls_720
+                },
+                "480": {
+                    src: ep.hls_480
                 }
-
-                returnedObject.files.push(res);
             }
-
-            return returnedObject;
         }
 
         return null;
